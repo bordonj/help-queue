0577b72544a834fc6905b47169584ab9bdb123f9
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-08 15:22:10 -0700

    update TicketDetail component to render SelectedTicket

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 function TicketDetail(props){
+  const { ticket } = props;
+
   return (
     <React.Fragment>
       <h1>Ticket Detail</h1>
+      <h3>{ticket.location} - {ticket.names}</h3>
+      <p><em>{ticket.issue}</em></p>
       <hr/>
     </React.Fragment>
   );
 }
+
+TicketDetail.propType = {
+  ticket: PropTypes.object
+};
 
 export default TicketDetail;
