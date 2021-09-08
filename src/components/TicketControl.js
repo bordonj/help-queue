baf3061faf2ee1e2169c615ca8606c38859b4f78
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-08 02:26:48 -0700

    addTicketButton  to TicketControl

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -13,10 +13,12 @@ class TicketControl extends React.Component {
 
   render(){
     let currentlyVisibleState = null;
+    let addTicketButton = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm />
     } else {
       currentlyVisibleState = < TicketList/>
+      addTicketButton = <button onClick={this.handleClick}>Add Ticket</button>
     }
     return (
       <React.Fragment>
