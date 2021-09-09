072d1276ba43954ed31e5f37e5d433e28923f53c
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-09 08:32:16 -0700

    add defaultValues for updating the form

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,21 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// uncontrolled way bc not controlling the state in the form
+// controlled way is recommended
+
 function ReusableForm(props) {
+  const startingNames = props.ticket ? props.ticket.names : '';
+  const startingLocation = props.ticket ? props.ticket.location : '';
+  const startingIssue = props.ticket ? props.ticket.issue : '';
+
   return (
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
         <input
           type='text'
           name='names'
-          placeholder='Pair Names' />
+          placeholder='Pair Names' 
+          defaultValue={startingNames}/>
         <input
           type='text'
           name='location'
-          placeholder='Location' />
+          placeholder='Location' 
+          defaultValue={startingLocation}/>
         <textarea
           name='issue'
-          placeholder='Describe your issue.' />
+          placeholder='Describe your issue.' 
+          defaultValue={startingIssue}/>
         <button type='submit'>{props.buttonText}</button>
       </form>
     </React.Fragment>
@@ -24,7 +34,8 @@ function ReusableForm(props) {
 
 ReusableForm.propTypes = {
   formSubmissionHandler: PropTypes.func,
-  buttonText: PropTypes.string
+  buttonText: PropTypes.string,
+  ticket: PropTypes.object
 };
 
 export default ReusableForm;
